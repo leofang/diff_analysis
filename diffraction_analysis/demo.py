9e1c14a7d63f2b02a0b3c205608c9b2eadf38e0e
Leo Fang <leofang@bnl.gov>
2019-02-01 23:28:35 -0500

    1D alignment seems to work

--- diffraction_analysis/demo.py
+++ diffraction_analysis/demo.py
@@ -1,8 +1,21 @@
 import scipy.io
+import numpy as np
+
 from alignment import get_edge_pos_1D
+from preprocessing import canonicalize_1D
 
 mat = scipy.io.loadmat("../test_data/unaligned_data.mat")
-result1 = get_edge_pos_1D(mat)
-result2 = get_edge_pos_1D(mat, fit=True)
-print(result1)
-print(result2)
+pos = get_edge_pos_1D(mat) # faster
+#pos = get_edge_pos_1D(mat, fit=True)
+
+diff_data = canonicalize_1D(mat['assembled_det_data'])
+print(diff_data.shape)
+
+shift = np.int(np.median(pos))-pos
+print(shift_max, shift_min)
+for i in range(diff_data.shape[0]):
+    diff_data[i] = np.roll(diff_data[i], shift[i], axis=0)
+shift_max = np.max(shift) # >0
+shift_min = np.min(shift) # <0
+new_diff_data = diff_data[:, shift_max:shift_min, :, :]
+print(new_diff_data.shape)
